cc7daa792b093151fe1aa758bce50761ce30ce9b
zackage <zack.tzeng@gmail.com>
2016-03-21 01:00:19 -0400

    fix zip function call

--- archive/zipper.R
+++ archive/zipper.R
@@ -1,3 +1,3 @@
 archive_run <- function(confidence_path, prediction_path, ch_index){
-  zip(paste("DREAMToronto_", ch_index, sep = ""), c(confidence_path, prediction_path))
+  zip(paste("DREAMToronto_", ch_index,".zip", sep = ""), c(confidence_path, prediction_path))
 }
