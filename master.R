3aaff50467632df29f52f0bd321d0860f82c4af3
zackage <zack.tzeng@gmail.com>
2016-03-21 18:49:18 -0400

    update master.R

--- master.R
+++ master.R
@@ -19,21 +19,21 @@ XVAL2TRAIN_RATIO <- 0.3
 # proportion of the training set used for cross validation
 # range: 0 to 1
 
-PCA_FEATURE_NAMES <- c()
+TOTAL_FEATURES <- c("monotherapy_normalized_avg_imputed", "drug_pchem", "string_gex", "triplets")
+
+PCA_FEATURES <- c("monotherapy_normalized_avg_imputed")
 # vector of features used to train model and predict result which require PCA
 # range: 
 
-NON_PCA_FEATURE_NAMES <- c()
-# vector of features used to train model and predict result which does not require PCA
-# range: 
+SWAP_FEATURES <- c("monotherapy_normalized_avg_imputed", "drug_pchem")
 
-MODEL_PARAM_START <- list()
+MODEL_PARAM_START <- list(hidden=c(300, 300, 300), epoch=1000)
 # list of model parameters that map to their corresponding value or range
 # the pipeline will find the optimal value for all parameter to train the model
 # range: 
 # Example: list(h=c(300,300,300), e=1000)
 
-MODEL_PARAM_END <- list()
+MODEL_PARAM_END <- list(hidden=c(300, 300, 300), epoch=1000)
 # list of model parameters that map to their corresponding value or range
 # the pipeline will find the optimal value for all parameter to train the model
 # range: 
@@ -45,7 +45,7 @@ COMPLETION_STAGE <- "S"
 #   T - training process
 #   P - predicting process
 #   F - formatting process
-#   S - submitting process
+#   A - archiving process
 
 
 # ========================================
@@ -80,12 +80,21 @@ source("/archive/zipper.R")
 
 
 # ========================================
-# INITIALIZE LOG
+# GLOBAL VARIABLE
+# ========================================
+
+log_count <- length(list.files("./log/logs/"))
+
+
+# ========================================
+# CREATE LOG FILE
 # ========================================
 # require to have master_log.csv and logs/ in Drug_Combo_Prediction/log/ before running this section
 
-# master_log_header <- paste("log id", "xval r2", "date", "log file path", "training time")
-######### Assume this file with header already exist!!!!!!!!!!#########################
+log_count <- log_count + 1
+log_file_path <- paste("log/logs/log", log_count, ".txt", sep = "")
+file.create(log_file_path)
+write(paste("log ID:\tlog", log_count, sep = ""), log_file_path, append = TRUE)
 
 
 # ========================================
@@ -98,27 +107,55 @@ load("/feature_bank/monotherapy_normalized_avg_imputed.RData")
 #   row - 1794 + 1089 + 31535 (training feature set + ch 1a feature set + ch 2 feature set without duplication)
 #   col - depends like the feature group
 
-t_xval_ch1_ch2_feature_set <- prep_run(PCA_FEATURE_NAMES, NON_PCA_FEATURE_NAMES)
-# prep_run returns a vector of four .csv file paths that store h2o formated matrices for training, xval, ch1 and ch2 feature sets
 
-######## MODIFY SO THAT WE DON'T PREP ALL THE MATRICES CUZ THERE IS THE STAGE CONFIG!!!!!!!
+t_xval_ch1_ch2_feature_set <- prep_run(TOTAL_FEATURES, PCA_FEATURES, SWAP_FEATURES, XVAL2TRAIN_RATIO, use_pred_for_pca, base_name)
+# input 
+#   TOTAL_FEATURES:   vector containing all the features for training
+#   PCA_FEATURES:     vector containing features to pca
+#   SWAP_FEATURES:    vector containing features to swap for symmetry
+#   XVAL2TRAIN_RATIO: proportion of cross validation set to original training set
+#   use_pred_for_pca: boolean value indicating whether prediction feature sets are involved in pca
+#   base_name:        base file name for prepared training, xval, ch1 and ch2 feature set
+# return
+#   vector of strings containing paths to the four h2o formatted .csv files, training, xval, ch1 and ch2
+# side effect
+#   1. save the four h2o formatted .csv files inside prep folder
 
 
 # ========================================
 # NEURAL NETWORK TRAINING
 # ========================================
 
-best_model <- train_run(t_xval_ch1_ch2_feature_set[1], t_xval_ch1_ch2_feature_set[2], MODEL_PARAM)
-# train_run takes 3 parameters: paths to the training and xval set and the model parameter list
-# train_run returns the dnn model that gives the best xval score
+best_model <- train_run(t_xval_ch1_ch2_feature_set[1], t_xval_ch1_ch2_feature_set[2], MODEL_PARAM_START, MODEL_PARAM_END, log_file_path, SWAP_FEATURES, TRUE)
+# input
+#   t_xval_ch1_ch2_feature_set[1]:  feature set for training models
+#   t_xval_ch1_ch2_feature_set[2]:  feature set for cross validating the trained model
+#   MODEL_PARAM_START:              start value for model training parameter range, same as end if it's a value
+#   MODEL_PARAM_END:                end value for model training parameter range, same as start if it's a value
+#   log_file_path:                  path to the log file for this training
+#   TRUE:                           boolean value indicating whether the training result should be saved in database
+# return
+#   trained h2o model 
+# side effect
+#   1. save the model in model folder of train folder
+#   2. log the result in master_log.csv and in a separate .txt formated log file
 
 
 # ========================================
 # PREDICTION
 # ========================================
 
-ch1_prediction_score <- predict_run(best_model, t_xval_ch1_ch2_feature_set[3])
-ch2_prediction_score <- predict_run(best_model, t_xval_ch1_ch2_feature_set[4])
+if(COMPLETION_STAGE!=T){
+  ch1_prediction_score <- predict_run(best_model, t_xval_ch1_ch2_feature_set[3])
+  ch2_prediction_score <- predict_run(best_model, t_xval_ch1_ch2_feature_set[4])
+}
+# input
+#   best_model:                     the trained h2o model for prediction
+#   t_xval_ch1_ch2_feature_set[3]:  feature set for prediction
+# return
+#   prediction score
+# side effect
+#   1. save prediction in pred folder
 
 
 # ========================================
@@ -132,10 +169,18 @@ ch2_formatted_prediction_score <- format_ch2(ch2_prediction_score, final = TRUE)
 # x determines the format: either challlenge 1 or challenge 2 format (ch1 or ch2)
 # final (boolean) indicates whether to format for the final round or for leaderboard rounds
 
+
 # ========================================
 # ARCHIVE
 # ========================================
 
-archive_run(ch1_confidence_path, ch1_formatted_prediction_path, 1)
-archive_run(ch2_confidence_path, ch2_formatted_prediction_path, 2)
-# input: confidence path, prediction path, challenge index (1 is 1a and 2 is 2)
+if(COMPLETION_STAGE=='A'){
+  archive_run(ch1_confidence_path, ch1_formatted_prediction_path, 1)
+  archive_run(ch2_confidence_path, ch2_formatted_prediction_path, 2)
+}
+# input
+#   ch1_confidence_path:            path to the confidence score .csv file
+#   ch1_formatted_prediction_path:  path to the properly formatted prediction .csv file
+#   1:                              integer value of either 1 or 2 indicating the challenge index for naming purposes
+# side effect
+#   1. save the zip file ready for submission in archive folder
